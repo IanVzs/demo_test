bf5d0abb57c74cc523eb7188bd36d634d71466ba
Ian <vzsxianggu@gmail.com>
2020-01-31 12:07:08 +0800

    add opencv_some

--- about_time_range.py
+++ about_time_range.py
@@ -9,7 +9,7 @@ def yield_get_dates(start, end, _format="%Y-%m-%d"):
         yield start.strftime(_format)
         start = start + datetime.timedelta(days=1)
 
-def get_date_steps(start, end, steps=None, _format="%Y-%m-%d"):
+def yield_get_date_steps(start, end, steps=None, _format="%Y-%m-%d"):
     _start = datetime.datetime.strptime(start, "%Y-%m-%d")
     start = datetime.datetime.strptime(start, "%Y-%m-%d")
     end = datetime.datetime.strptime(end, "%Y-%m-%d")
@@ -19,16 +19,20 @@ def get_date_steps(start, end, steps=None, _format="%Y-%m-%d"):
         if num > len(steps) - 1:
             break
         step = datetime.datetime.strptime(steps[num], "%Y-%m-%d")
-        if _start >= step:
+        if _start >= step and start < step:
             num += 1
             yield (start.strftime(_format), step.strftime(_format))
             start = step + datetime.timedelta(days=1)
+        elif _start >= step:
+            num += 1
         else:
             _start = _start + datetime.timedelta(days=1)
     yield (start.strftime(_format), end.strftime(_format))
 
 [print(i) for i in yield_get_dates("2020-04-27", "2020-05-23")]
 print("#----"*12)
-[print(i) for i in get_date_steps("2020-04-27", "2020-05-23")]
+[print(i) for i in yield_get_date_steps("2020-04-27", "2020-05-23")]
 print("#----"*12)
-[print(i) for i in get_date_steps("2020-04-27", "2020-05-23", ("2020-05-03","2020-05-10"))]
+[print(i) for i in yield_get_date_steps("2020-04-27", "2020-05-23", ("2020-05-03","2020-05-10"))]
+print("#----"*12)
+[print(i) for i in yield_get_date_steps("2020-04-27", "2020-01-23", ("2020-03-01","2020-05-01"))]
