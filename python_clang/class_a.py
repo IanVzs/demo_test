163f8afbd461b63b2923d3b26205b30d5f5b7ede
Ian <ianvzs@outlook.com>
2020-12-01 17:45:24 +0800

    add image get

--- python_clang/class_a.py
+++ python_clang/class_a.py
@@ -1,7 +1,21 @@
 class A:
     a = 1
+    def __init__(self):
+        self.ini = 'ini'
+
     def hi(self, w):
         print(self.a)
         print(w)
+
+    @classmethod
+    def cm(self):
+        print(f"in www: {self.ini}")
+
+    @classmethod
+    def cm2(hi):
+        print(f"in www2: asd")
+
 if "__main__" == __name__:
     A().hi("w is me")
+    A.cm2()
+    A().cm()
