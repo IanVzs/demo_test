d70c6243f342cfcf3728c304fd8f1aea2a7aecd5
Ian <ianvzs@outlook.com>
2020-12-28 18:33:59 +0800

    add time

--- python_clang/day_some.py
+++ python_clang/day_some.py
@@ -2,13 +2,22 @@ import time
 import datetime
 
 today = datetime.datetime.today()
+
+# datetime->str
 today_str = today.strftime("%Y-%m-%d")
+
+# str->datetime
 today = datetime.datetime.strptime(today_str, "%Y-%m-%d")
 print(f"year: {today.year}, month: {today.month}, day: {today.day}")
+
+# unixInt->datetime
 utoday = time.mktime(today.timetuple())
 utoday_str = time.strftime('%Y-%m-%d',time.localtime(utoday))
-
 print(today, today_str, utoday, utoday_str)
+
+# datetime->unixInt
+uintnnum = time.mktime(today.timetuple())
+print('uintnnum:', uintnnum)
 
 ttoday = datetime.datetime.today() + datetime.timedelta(days=1)
 ttoday_str = ttoday.strftime("%Y-%m-%d")
