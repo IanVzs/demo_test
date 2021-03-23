42e67b8a3cd85388abfe68c04e451a11b9709b30
Ian <ianvzs@outlook.com>
2021-03-23 10:37:30 +0800

    add debug

--- go_lang/README.md
+++ go_lang/README.md
@@ -1,3 +1,8 @@
 ## 测试
 需要一个golang 的测试例子
 如: `go test xxx_test.go`
+
+## DEBUG
+```bash
+go build -gcflags "-N -l" main.go
+```
--- python_clang/random_hanzi.py
+++ python_clang/random_hanzi.py
@@ -29,7 +29,17 @@ def random_words(num):
         words.append(chr(val))
     return ''.join(words)
 
+def random_items(all: list, rate: int=5):
+    new = []
+    for i in all:
+        if random.random() < (rate * 0.1):
+            new.append(i)
+        else:
+            continue
+    return new
+
 if __name__ == "__main__":
     user_id = "23"
     print(unicode_name(user_id))
     print(random_words(7))
+    print(random_items(unicode_name(user_id)))
