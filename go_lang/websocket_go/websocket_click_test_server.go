cfc496e34c23d5c4bfe44bf97cda8317c4305161
Ian <ianvzs@outlook.com>
2021-04-15 18:30:41 +0800

    websorcket about

---
+++ go_lang/websocket_go/websocket_click_test_server.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = flag.String("addr", "127.0.0.1:8000", "http service address")
+var msg = flag.String("msg", "time", "this client send msg.")
+
+func init() {
+	flag.Parse()
+}
+
+func main() {
+	// go mainWsEcho()
+	mainWsChat()
+}
+
+func mainWsChat() {
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws_chat", RawQuery: "chat_id=123"}
+	// u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws_chat"}
+	var dialer *websocket.Dialer
+
+	conn, _, err := dialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go timeWriter(conn)
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			return
+		}
+
+		fmt.Printf("received: %s\n", message)
+	}
+}
+
+func mainWsEcho() {
+	// u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	u := url.URL{Scheme: "ws", Host: *addr, Path: "/ws"}
+	var dialer *websocket.Dialer
+
+	conn, _, err := dialer.Dial(u.String(), nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	go timeWriter(conn)
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("read:", err)
+			return
+		}
+
+		fmt.Printf("received: %s\n", message)
+	}
+}
+
+func timeWriter(conn *websocket.Conn) {
+	for {
+		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Microsecond * 1)
+		fmt.Println("123")
+		sendMsg := *msg
+		if sendMsg == "time" {
+			conn.WriteMessage(websocket.TextMessage, []byte(time.Now().Format("2006-01-02 15:04:05")))
+		} else {
+			conn.WriteMessage(websocket.TextMessage, []byte(sendMsg))
+		}
+
+	}
+}
